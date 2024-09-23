26c0b60ca898ef4cc5e771259a0fbd0d6e400ea7
Oleksandr Ursatyi <24843887+aursaty@users.noreply.github.com>
2024-09-23 20:26:27 +0300

    Update main.py to call two requests, use utils module

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import requests
 import typer
 from pathlib import Path
-# from client.utils import save_results
+from utils import extract_and_store_predictions, save_image
 
 app = typer.Typer()
 
 @app.command()
 def predict(image_path: str):
-    print(image_path)
-    url = "http://localhost:8000/predict/"
+    url = "http://127.0.0.1:8000/predictClassesJson/"
     with open(image_path, "rb") as image_file:
-        files = {"file": (Path(image_path).name, image_file, "image/jpeg")}
+        files = {"file": (Path(image_path).name, image_file, "image/jpg")}
         response = requests.post(url, files=files)
+        response.raise_for_status
         results = response.json()
-        print(results)
+        if 200 <= response.status_code <= 299:
+            extract_and_store_predictions(results['result'])
+    
+    url = "http://127.0.0.1:8000/predictClassesOnImage/"
+    with open(image_path, "rb") as image_file:
+        files = {"file": (Path(image_path).name, image_file, "image/jpg")}
+        response = requests.post(url, files=files)
+        response.raise_for_status
+        if 200 <= response.status_code <= 299:
+            save_image(response.content)
 
 if __name__ == "__main__":
     app()
